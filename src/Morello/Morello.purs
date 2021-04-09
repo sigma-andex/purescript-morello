2414c7d504b8f19c0f4afaef8dc22f44e8c16a6c
sigma-andex <sigma.andex@pm.me>
2021-04-09 20:48:43 +0100

    Remove heterogeneous records as it lives now in heterogeneous-extrablatt

--- src/Morello/Morello.purs
+++ src/Morello/Morello.purs
@@ -1,11 +1,9 @@
 module Morello.Morello
   ( module Morello.Morello.Core
-  , module Morello.Morello.Record
   , module Morello.Morello.Validated
   , module Morello.Morello.Simple
   ) where
 
 import Morello.Morello.Core (Key, applyTemplate, blossom, branch, cherry, dual, key, pick, pick', core, (|>), (🌱), (🌸), (🍒))
 import Morello.Morello.Simple (Validated, Validate, Pick, ValidationError(..))
-import Morello.Morello.Record (HMapRec(..), HMapKRec(..), HSequenceRec(..), hmapRec, hmapKRec, hsequenceRec)
 import Morello.Morello.Validated (ValidateE, ValidatedE, invalid, valid, as, asIs)
