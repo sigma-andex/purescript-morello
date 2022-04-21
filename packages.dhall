d8018f2c1b6c6e883a6b2084c38ca7c06d00a32c
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-04-21 15:36:38 +0100

    Update for 0.15
    - Add updated package set
    - Fix barlow lens breaking changes
    - Switch pick' and pick

--- packages.dhall
+++ packages.dhall
@@ -1,4 +1,68 @@
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.14.0-20210406/packages.dhall sha256:7b6af643c2f61d936878f58b613fade6f3cb39f2b4a310f6095784c7b5285879
+      https://raw.githubusercontent.com/purescript/package-sets/prepare-0.15/src/packages.dhall
+        sha256:b1c6d06132b7cbf1e93b1e5343044fba1604b50bfbe02d8f80a3002e71569c59
 
 in  upstream
+  with metadata.version = "v0.15.0-alpha-05"
+  with spec =
+    { repo = "https://github.com/purescript-spec/purescript-spec.git"
+    , version = "v6.0.0"
+    , dependencies =
+      [ "aff"
+      , "ansi"
+      , "avar"
+      , "console"
+      , "exceptions"
+      , "foldable-traversable"
+      , "fork"
+      , "now"
+      , "pipes"
+      , "prelude"
+      , "strings"
+      , "transformers"
+      ]
+    }
+  with spec-discovery =
+    { repo = "https://github.com/purescript-spec/purescript-spec-discovery.git"
+    , version = "v7.0.0"
+    , dependencies =
+      [ "aff"
+      , "aff-promise"
+      , "arrays"
+      , "console"
+      , "effect"
+      , "foldable-traversable"
+      , "node-fs"
+      , "prelude"
+      , "spec"
+      ]
+    }
+  with variant =
+    { dependencies =
+      [ "enums"
+      , "lists"
+      , "maybe"
+      , "partial"
+      , "prelude"
+      , "record"
+      , "tuples"
+      , "unsafe-coerce"
+      ]
+    , repo =
+        "https://github.com/working-group-purescript-es/purescript-variant.git"
+    , version = "v0.15.0-update"
+    }
+  with heterogeneous =
+    { dependencies =
+      [ "either", "functors", "prelude", "record", "tuples", "variant" ]
+    , repo = "https://github.com/natefaubion/purescript-heterogeneous.git"
+    , version = "v0.5.1"
+    }
+  with heterogeneous-extrablatt =
+    { dependencies =
+      [ "console", "effect", "heterogeneous", "spec", "spec-discovery" ]
+    , version = "v0.1.0"
+    , repo =
+        "https://github.com/sigma-andex/purescript-heterogeneous-extrablatt.git"
+    }
+  
--- src/Morello/Morello/Core.purs
+++ src/Morello/Morello/Core.purs
@@ -3,7 +3,10 @@ module Morello.Morello.Core where
 import Control.Semigroupoid (compose)
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Lens (AGetter', Forget, view)
-import Data.Lens.Barlow (class Barlow, class ConstructBarlow, class ParseSymbol, barlow)
+import Data.Lens.Barlow (class Barlow, barlow)
+import Data.Lens.Barlow.Construction (class ConstructBarlow)
+import Data.Lens.Barlow.Parser (class ParseSymbol)
+import Data.Lens.Barlow.Types (TList)
 import Data.Profunctor.Strong ((&&&))
 import Data.Traversable (class Traversable, traverse)
 import Data.Tuple (Tuple(..), fst, snd, uncurry)
@@ -93,16 +96,18 @@ blossom = snd
 
 infixr 8 blossom as 🌸
 
-pick :: forall s a err b. AGetter' s a -> ValidateE a err b -> PickE s err b
-pick lens validate = PickE (view lens >>> validate)
+pick' :: forall s a err b. AGetter' s a -> ValidateE a err b -> PickE s err b
+pick' lens validate = PickE (view lens >>> validate)
 
-pick' :: forall err output s sym tlist input. ParseSymbol sym tlist => ConstructBarlow tlist (Forget input) { | s } input => Proxy sym -> ValidateE input err output -> PickE { | s } err output
-pick' proxy = pick (barlow proxy)
+--pick' :: forall err output s sym tlist input. ParseSymbol sym tlist => ConstructBarlow tlist (Forget input) { | s } input => Proxy sym -> ValidateE input err output -> PickE { | s } err output
+pick ∷ ∀ (s31 ∷ Type) (err33 ∷ Type) (b34 ∷ Type) (b36 ∷ Type) (string39 ∷ Symbol) (t43 ∷ TList). ParseSymbol string39 t43 ⇒ ConstructBarlow t43 (Forget b36) s31 s31 b36 b36 ⇒ Proxy string39 → (b36 → V (NonEmptyArray err33) b34) → PickE s31 err33 b34
+pick proxy = pick' (barlow proxy)
 
-core :: forall f s a err b. Traversable f => AGetter' s (f a) -> ValidateE a err b -> PickE s err (f b)
-core lens validate = PickE (view lens >>> traverse validate)
+core' :: forall f s a err b. Traversable f => AGetter' s (f a) -> ValidateE a err b -> PickE s err (f b)
+core' lens validate = PickE (view lens >>> traverse validate)
 
-core' :: forall f input err output sym tlist s. Traversable f => ParseSymbol sym tlist => ConstructBarlow tlist (Forget (f input)) { | s } (f input) => Proxy sym -> ValidateE input err output -> PickE { | s } err (f output)
-core' proxy = core (barlow proxy)
+--core' :: forall f input err output sym tlist s. Traversable f => ParseSymbol sym tlist => ConstructBarlow tlist (Forget (f input)) { | s } (f input) => Proxy sym -> ValidateE input err output -> PickE { | s } err (f output)
+core ∷ ∀ (f105 ∷ Type -> Type) (s106 ∷ Type) (a107 ∷ Type) (err108 ∷ Type) (b109 ∷ Type) (string114 ∷ Symbol) (t118 ∷ TList). Traversable f105 ⇒ ParseSymbol string114 t118 ⇒ ConstructBarlow t118 (Forget (f105 a107)) s106 s106 (f105 a107) (f105 a107) ⇒ Proxy string114 → (a107 → V (NonEmptyArray err108) b109) → PickE s106 err108 (f105 b109)
+core proxy = core' (barlow proxy)
 
 infixr 9 compose as |>
